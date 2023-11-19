c37212f8400cecd6d29707787ce164ce669d3cc2
matias <maty_colazo@hotmail.com>
2023-11-19 17:53:59 -0300

    componentes, arreglos y model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-votos';
+  
+  addLink(title:HTMLInputElement, link:HTMLInputElement){
+    console.log('valores: ', title.value, link.value);
+    return false
+  }
 }
